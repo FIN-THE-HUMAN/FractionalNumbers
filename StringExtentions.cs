d1243c28c54ca1d7094d1c328bdc52ccc72d6fc6
FIN-THE-HUMAN <malyshevi@yandex.ru>
2018-11-12 14:46:14 +0300

    Переделан метод Slice

--- StringExtentions.cs
+++ StringExtentions.cs
@@ -13,37 +13,33 @@ namespace Encryption
         /// </summary>
         private static T[] ArrayWith<T>(this T[] array, T item)
         {
-            T[] buffer = new T[array.Length];
-            Array.Copy(array, buffer, array.Length);
-            T[] result = new T[buffer.Length + 1];
-            Array.Copy(buffer, result, buffer.Length);
+            T[] result = new T[array.Length + 1];
+            Array.Copy(array, result, array.Length);
             result[result.Length - 1] = item;
+            return result;
+        }
+
+        private static List<int> IndexesOf(this string str, char c)
+        {
+            List<int> result = new List<int>();
+            for (int i = 0; i < str.Length; i++)
+                if(str[i] == c) result.Add(i);
             return result;
         }
 
         public static string[] Slice(this string str, char devider)
         {
-            int start = 0;
-            string[] result = new string[0];
-            string[] buffer = new string[0];
-            for(int i = 0; i < str.Length; i++)
-            {
-                if (str[i] == devider)
-                {
-                    result = result.ArrayWith(str.Substring(start, i - start));
-                    start = i + 1;
-                }
-            }
+            List<int> indexesOfDeviders = str.IndexesOf(devider);
+            if (indexesOfDeviders.Count == 0) return new string[] { str };
 
-            if (start == 0)
+            string[] result = new string[indexesOfDeviders.Count + 1];
+
+            result[0] = str.Substring(0, indexesOfDeviders[0]);
+            for (int i = 1; i < indexesOfDeviders.Count; i++)
             {
-                result = new string[1];
-                result[0] = str;
+                result[i] = str.Substring(indexesOfDeviders[i - 1] + 1, indexesOfDeviders[i] - indexesOfDeviders[i - 1] - 1);
             }
-            else
-            {
-                result = result.ArrayWith(str.Substring(start, str.Length - start));
-            }
+            result[indexesOfDeviders.Count] = str.Substring(indexesOfDeviders[indexesOfDeviders.Count - 1] + 1, str.Length - indexesOfDeviders[indexesOfDeviders.Count - 1] - 1);
 
             return result;
         }
